14794a0e1187616ddbed6e25a84b1d48eb41196a
Unknown <blackcodesun@gmail.com>
2018-07-11 16:25:16 -0700

    Comment/Naming Fixes

--- add-font.ps1
+++ add-font.ps1
@@ -1,6 +1,7 @@
 # add-font.ps1
 # Mike Stine, 7/11/2018
-# Window recognizes fonts you drag and drop into the windows font folder, but does not recognize fonts copied into the folder through cli or script. BECAUSE, the windows explorer shell triggers a registration process through the Shell.Application COM object.  And so will we.
+# This powershell script installs a font file into Windows
+# Window recognizes fonts dragged and dropped into the Windows font folder, but does not recognize fonts copied through cli or script. This is due to the windows explorer shell triggering a font registration process.  This script evokes the same windows explorer shell trigger to install fonts
 
 # This is the folder that contains the font(s) to install, or this can be a direct path to a font
 $srcPath = "C:\temp\add-font\*"
@@ -11,15 +12,15 @@ $fontFiles = Get-ChildItem -Path "$srcPath" -Include *.fon,*.fnt,*.otf,*.ttc,*.t
 # Creates new Shell.Application COM object
 $shellObject = New-Object -ComObject Shell.Application
 
-# Sets its namespace to the CLSID hex '0X14' (Windows font folder and registration trigger)
-$windowsFontFolderTrigger = $shellObject.Namespace(0x14)
+# Sets object's namespace to CLSID hex '0X14' (Windows font folder and registration trigger)
+$windowsFontFolder = $shellObject.Namespace(0x14)
 
 # Move and register font files.
 foreach($fontFile in $fontFiles) {
   # test if font is already installed 
   if ( !(Test-Path "${env:windir}\Fonts\$($fontFile.Name)") ) {
     # install font
-    $windowsFontFolderTrigger.CopyHere($fontFile.fullname)
+    $windowsFontFolder.CopyHere($fontFile.fullname)
   }
 }
 
